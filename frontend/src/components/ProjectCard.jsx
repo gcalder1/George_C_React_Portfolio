d30acf00fc0554253126a311e1df1b631e122ac7
unknown <george.calderon2000@gmail.com>
2025-08-25 23:24:52 -0400

    editted project cards to resemble figma

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,9 +1,9 @@
 export const ProjectCard = ({ imageContent, altContent, imageStyling, cardDestination, cardContent }) => {
     return (
         <a href={cardDestination} target="_blank" rel="noopener noreferrer">
-            <div className="flex flex-row items-center bg-[#BFC0CF] rounded-xl shadow-md overflow-hidden mb-6 hover:shadow-xl transition duration-300">
-                <div>
-                    <img src={imageContent} alt={altContent} className={`w-2/3 h-64 object-cover ${imageStyling}`}/>
+            <div className="flex flex-row items-start bg-[#BFC0CF] rounded-xl shadow-md overflow-hidden mb-6 hover:shadow-xl transition duration-300">
+                <div className="flex-shrink-0">
+                    <img src={imageContent} alt={altContent} className={`h-64 w-auto object-cover ${imageStyling}`}/>
                 </div>
                 <div className="w-1/3 p-6 h-64 overflow-y-auto">
                     <p>{cardContent}</p>
