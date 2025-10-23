c21a488d0dedd328564553f9d732dc9026a23171
unknown <george.calderon2000@gmail.com>
2025-10-23 17:56:45 -0400

    fixed description placements for projects

--- frontend/src/PageComponents/Projects.jsx
+++ frontend/src/PageComponents/Projects.jsx
@@ -18,14 +18,14 @@ export const Projects = () => {
                 altContent={"The Silly Corner"} 
                 imageStyling={""} 
                 cardDestination={"https://clever-alpaca-c54b17.netlify.app/"} 
-                cardContent={"Tidy Bloom is designed to help people with executive function challenges, busy schedules, or children who struggle with starting and completing tasks. The app guides users to pick a room or self-care routine, start a short focus timer, and earn points and streaks as they make progress. Each effort grows a plant, eventually cultivating a full garden, turning everyday chores into a motivating and rewarding experience. The design is gentle and supportive, reducing choice overload and lowering the energy needed to get started. Tidy Bloom reframes mundane tasks into something fun, emphasizing consistency over perfection."} 
+                cardContent={"The Silly Corner is a web application that delivers jokes using the JokeAPI, a public API offering over 1,000 consistently formatted jokes. Users can view jokes across five safe-for-work categories: Pun, Spooky, Christmas, Programming, and Miscellaneous. The project leverages fetch requests to display jokes in real-time, providing instant feedback and interaction. Built with Node.js and Vite, the app emphasizes accessibility and ease of use while showcasing API integration in a fun, lighthearted way."} 
                 />
                 <ProjectCard 
                 imageContent={"/projectImagesFolder/tidybloomDashboard.png"} 
                 altContent={"TidyBloom"} 
                 imageStyling={""} 
                 cardDestination={"https://github.com/gcalder1/TKH_A_Little_Friendly_Competition_Submission"} 
-                cardContent={"The Silly Corner is a web application that delivers jokes using the JokeAPI, a public API offering over 1,000 consistently formatted jokes. Users can view jokes across five safe-for-work categories: Pun, Spooky, Christmas, Programming, and Miscellaneous. The project leverages fetch requests to display jokes in real-time, providing instant feedback and interaction. Built with Node.js and Vite, the app emphasizes accessibility and ease of use while showcasing API integration in a fun, lighthearted way."} 
+                cardContent={"Tidy Bloom is designed to help people with executive function challenges, busy schedules, or children who struggle with starting and completing tasks. The app guides users to pick a room or self-care routine, start a short focus timer, and earn points and streaks as they make progress. Each effort grows a plant, eventually cultivating a full garden, turning everyday chores into a motivating and rewarding experience. The design is gentle and supportive, reducing choice overload and lowering the energy needed to get started. Tidy Bloom reframes mundane tasks into something fun, emphasizing consistency over perfection."} 
                 />
                 
             </div>
